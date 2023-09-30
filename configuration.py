92471fe329e02ae853cdd16e7d5e300e24731cbd
anna-venice <146595921+anna-venice@users.noreply.github.com>
2023-10-01 02:22:04 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://5c8fc907-8c3f-4283-8dcf-317df39f3c45.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
